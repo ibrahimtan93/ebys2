52ad31ae2928a3d483afd55a76a81d5012c7bd2f
mcanuste <muratcanuste@gmail.com>
2016-12-07 20:57:41 +0300

    viewMidtermMarkTest_added

--- src/test/java/com/tdd/ebys2/StudentTest.java
+++ src/test/java/com/tdd/ebys2/StudentTest.java
@@ -1,7 +1,24 @@
 package com.tdd.ebys2;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
+ * TODO View course mark
+ * -TODO View midterm mark
+ * -TODO View final mark
+ * TODO View a term's grade point avarage
+ * TODO View cumulative grade point avarage
+ *
  * Created by darthvader on 07.12.2016.
  */
 public class StudentTest {
+    @Test
+    public void viewMidtermMark(){
+        Student student = new Student();
+        Course course = new Course();
+        course.midtermMark = 50;
+
+        assertEquals(50, course.midtermMark);
+    }
 }
