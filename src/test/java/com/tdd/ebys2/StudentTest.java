8bee0b8d4818f9c5000863b6e6bf806765a1b2c9
mcanuste <muratcanuste@gmail.com>
2016-12-07 21:12:42 +0300

    viewCourseMark_normalized

--- src/test/java/com/tdd/ebys2/StudentTest.java
+++ src/test/java/com/tdd/ebys2/StudentTest.java
@@ -7,27 +7,23 @@ import static org.junit.Assert.*;
  * TODO View course mark
  * -TODO View midterm mark #
  * -TODO View final mark #
+ * -TODO Normalize viewing course marks #
+ * -TODO View course's term mark
  * TODO View a term's grade point avarage
  * TODO View cumulative grade point avarage
+ *
  *
  * Created by darthvader on 07.12.2016.
  */
 public class StudentTest {
     @Test
-    public void viewMidtermMark(){
+    public void viewCourseMark(){
         Student student = new Student();
         Course course = new Course();
         course.setMidtermMark(50);
-
-        assertEquals(50, course.getMidtermMark());
-    }
-
-    @Test
-    public void viewFinalMark(){
-        Student student = new Student();
-        Course course = new Course();
         course.setFinalMark(100);
 
+        assertEquals(50, course.getMidtermMark());
         assertEquals(100, course.getFinalMark());
     }
 }
