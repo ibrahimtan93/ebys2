adb3c9450316a92b8eeaca87c34992f4649aa4ff
mcanuste <muratcanuste@gmail.com>
2016-12-07 22:56:40 +0300

    SameCourseActivityException

--- src/test/java/com/tdd/ebys2/TeacherTest.java
+++ src/test/java/com/tdd/ebys2/TeacherTest.java
@@ -1,5 +1,6 @@
 package com.tdd.ebys2;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -9,18 +10,37 @@ import static org.junit.Assert.*;
  * --- TODO More than one activity #
  * - TODO Specify the percentage of the activities #
  * ---TODO CourseActivity class that holds the activity name and percentage #
+ * ---TODO Make sure that same activity cant exist
+ * -----TODO Same Activity Object checking
+ * -----TODO Same Activity Name checking
+ * -----TODO Create SameCourseActivityException class
+ * ---TODO Make sure that all percentages sums up to 100
  *
  * Created by darthvader on 07.12.2016.
  */
 public class TeacherTest {
+    Teacher teacher;
+    Course course;
+    @Before
+    public void setup(){
+        teacher = new Teacher();
+        course = new Course();
+    }
+
     @Test
     public void specifyCourseActivities(){
-        Teacher teacher = new Teacher();
-        Course course = new Course();
         CourseActivity activity = new CourseActivity("midtermExam", 30);
         course.addActivity(activity);
 
         assertTrue(course.containsActivity(activity));
         assertFalse(course.containsActivity(new CourseActivity("finalExam", 60)));
+    }
+
+    @Test(expected = SameCourseActivityException.class)
+    public void insertCheckSameActivityObject(){
+        CourseActivity activity = new CourseActivity("midtermExam", 30);
+
+        course.addActivity(activity);
+        course.addActivity(activity);
     }
 }
