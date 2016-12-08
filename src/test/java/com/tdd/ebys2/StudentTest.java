46f5de32528ec3006e305087745c1d0b065e0d3b
mcanuste <muratcanuste@gmail.com>
2016-12-08 05:12:37 +0300

    Student_enrollment_testAdded

--- src/test/java/com/tdd/ebys2/StudentTest.java
+++ src/test/java/com/tdd/ebys2/StudentTest.java
@@ -1,5 +1,6 @@
 package com.tdd.ebys2;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -9,12 +10,29 @@ import static org.junit.Assert.*;
  * -TODO View final mark #
  * -TODO Normalize viewing course marks #
  * -TODO View course's term mark #
- * TODO Transfer Course related tests to CourseTest
- * TODO View a term's grade point avarage
- * TODO View cumulative grade point avarage
+ * TODO Transfer Course related tests to CourseTest #
+ * TODO View a term's grade point avarage #
+ * TODO View cumulative grade point avarage #
  *
  *
  * Created by darthvader on 07.12.2016.
  */
 public class StudentTest {
+    private Student student;
+    private Course course;
+
+    @Before
+    public void setup() throws CourseActivityException {
+        student = new Student();
+        course = new Course();
+        course.addActivity(CourseActivity.midtermExam(40));
+        course.addActivity(CourseActivity.finalExam(60));
+    }
+
+    @Test
+    public void enrollmentTest(){
+        student.enroll(course);
+
+        assertTrue(student.isEnrolled(course));
+    }
 }
