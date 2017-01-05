879df57e9b122e6081a9bd709e855e943642fd56
mcanuste <muratcanuste@gmail.com>
2017-01-05 04:11:23 +0300

    enrollmentTest_refactor

--- src/test/java/com/tdd/ebys2/StudentTest.java
+++ src/test/java/com/tdd/ebys2/StudentTest.java
@@ -20,16 +20,26 @@ import static org.junit.Assert.*;
  * --TODO student can only enroll to the available courses
  * ---TODO Course term is equal or below students term
  * TODO Calculate term grade average
+ * TODO Call course from semester with index or object
+ * --TODO Needs equals method
  *
  * Created by darthvader on 07.12.2016.
  */
 public class StudentTest {
+
+    //**************************************** Fixture Methods *************************************
     private Student student;
     private Course course;
     private Semester semester;
 
+    /**
+     * TODO Use mock objects for setup.
+     *
+     * @throws CourseActivityException
+     */
     @Before
     public void setup() throws CourseActivityException {
+        //Fixture setup..
         semester = new Semester();
         student = new Student();
         course = new Course("TDD");
@@ -38,12 +48,25 @@ public class StudentTest {
         semester.addCourse(course);
     }
 
-    @Test(expected = NullPointerException.class)
+
+    //**************************************** Test Methods *************************************
+    /**
+     * TODO Two different condition. #
+     * TODO Use exception pattern for readability. #
+     * TODO Assertion Msg smell.  #
+     */
+    @Test
     public void enrollmentTest(){
+        //Fixture setup..
         student.enroll(semester.getCourse("TDD"));
 
-        assertTrue(student.isEnrolled(course));
-
-        student.enroll(semester.getCourse("Algorithms"));
+        //Verification..
+        try{
+            student.enroll(semester.getCourse("Algorithms"));
+            fail("Failed to throw exception when trying to enroll to a nonexistent course.");
+        }
+        catch (Exception e){
+            assertTrue(e instanceof NullPointerException);
+        }
     }
 }
