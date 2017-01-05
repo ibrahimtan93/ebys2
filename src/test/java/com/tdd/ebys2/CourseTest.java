1c4071e8ab29925843275f2977cab9db1dd1a6d0
mcanuste <muratcanuste@gmail.com>
2017-01-05 05:08:48 +0300

    courseTest_refactor

--- src/test/java/com/tdd/ebys2/CourseTest.java
+++ src/test/java/com/tdd/ebys2/CourseTest.java
@@ -6,85 +6,152 @@ import static org.junit.Assert.*;
 
 /**
  * TODO Get all tests related to Courses to this test class #
- * TODO viewCourseMarkTest refactoring
+ * TODO viewCourseMarkTest refactoring #
  * TODO calculateTermGrade
+ * TODO Limit CourseActivity percentage between 0 and 100.
+ * TODO Create Grade or Mark object.
  *
  * Created by darthvader on 08.12.2016.
  */
 public class CourseTest {
+    //**************************************** Fixture *************************************
     private Course course;
+    private CourseActivity activityMidterm;
+    private CourseActivity activityFinal;
 
     @Before
     public void setup(){
+        //Fixture setup..
         course = new Course("TDD");
+        activityMidterm = CourseActivity.midtermExam(40);
+        activityFinal= CourseActivity.finalExam(60);
     }
 
+
+    /**
+     * TODO Same fixture. Move CourseActivity objects to setup method. #
+     */
+    //**************************************** Verification *************************************
+
+    /**
+     *  TODO Use mock objects.
+     *  TODO Assetion msg smell. #
+     *
+     * @throws CourseActivityException
+     */
     @Test
     public void addActivityTest() throws CourseActivityException {
-        CourseActivity activity = CourseActivity.midtermExam(40);
-        CourseActivity activity2 = CourseActivity.finalExam(60);
-        course.addActivity(activity);
-        course.addActivity(activity2);
+        //Fixture setup..
+        course.addActivity(activityMidterm);
 
-        assertTrue(course.containsActivityType(activity));
-        assertTrue(course.containsActivityType(activity2));
+        //Verification..
+        assertTrue("Failed to add CourseActivity to Course.", course.containsActivityType(activityMidterm));
     }
 
-    @Test(expected = CourseActivityException.class)
-    public void addSameActivityTest() throws CourseActivityException {
-        CourseActivity activity = CourseActivity.midtermExam(40);
-        CourseActivity activity2 = CourseActivity.midtermExam(30);
-        course.addActivity(activity);
-        course.addActivity(activity2);
+    /**
+     *  TODO Exception pattern #
+     *  TODO Assetion msg smell. #
+     *
+     * @throws CourseActivityException
+     */
+    @Test
+    public void addActivityTest_sameActivityException() {
+        //Verification..
+        try{
+            course.addActivity(activityMidterm);
+            course.addActivity(activityMidterm);
+            fail("Failed to throw exception when trying to add same CourseActivity object to Course more than once.");
+        }
+        catch (Exception e){
+            assertTrue(e instanceof CourseActivityException);
+        }
     }
 
-    @Test(expected = CourseActivityException.class)
-    public void addActivityPercentageTest() throws CourseActivityException {
-        CourseActivity activity = CourseActivity.midtermExam(50);
-        CourseActivity activity2 = CourseActivity.finalExam(60);
-        course.addActivity(activity);
-        course.addActivity(activity2);
+    /**
+     *  TODO Assetion msg smell. #
+     *  TODO Exception pattern #
+     *
+     * @throws CourseActivityException
+     */
+    @Test
+    public void addActivityTest_percentageException() {
+        //Verification..
+        try{
+            course.addActivity(activityFinal);
+            course.addActivity(activityFinal);
+            fail("Failed to throw CourseActivityException when trying to add activities whose percentage sum excels 100.");
+        }
+        catch (Exception e){
+            assertTrue(e instanceof CourseActivityException);
+        }
     }
 
+    /**
+     * TODO Assertion Msg. #
+     * TODO Use object equals. #
+     * TODO Use mock objects.
+     */
     @Test
     public void assignTeacherToCourse(){
+        //Fixture setup..
         Teacher teacher = new Teacher("Oguz Dikenelli");
         course.setTeacher(teacher);
 
-        assertEquals("Oguz Dikenelli",course.getTeacher().getName());
+        //Verification..
+        assertEquals("Failed to assign Teacher to Course.", teacher,course.getTeacher());
     }
 
+    /**
+     *  TODO Refactor fixture setup #
+     *  TODO Assertion Msg. #
+     *  TODO Use mock objects.
+     *
+     * @throws CourseActivityException
+     */
     @Test
     public void viewCourseActivityMarkTest() throws CourseActivityException {
-        course.addActivity(CourseActivity.midtermExam(40));
-        course.addActivity(CourseActivity.finalExam(60));
+        //Fixture setup..
+        activityMidterm.setMark(50);
+        course.addActivity(activityMidterm);
 
-        course.getActivity("MidtermExam").setMark(50);
-        course.getActivity("FinalExam").setMark(100);
-
-        assertEquals(50, course.getActivity("MidtermExam").getMark(), 0.02);
-        assertEquals(100, course.getActivity("FinalExam").getMark(), 0.02);
+        //Verification..
+        assertEquals("Failed to view CourseActivity mark.",50, course.getActivity("MidtermExam").getMark(), 0.02);
     }
 
+    /**
+     * TODO Assertion Msg. #
+     * TODO Fixture refactor. #
+     *
+     * @throws CourseActivityException
+     */
     @Test
     public void calculateTermMarkTest() throws CourseActivityException {
-        course.addActivity(CourseActivity.midtermExam(40));
-        course.addActivity(CourseActivity.finalExam(60));
+        //Fixture setup..
+        activityMidterm.setMark(80);
+        activityFinal.setMark(100);
+        course.addActivity(activityMidterm);
+        course.addActivity(activityFinal);
 
-        course.getActivity("MidtermExam").setMark(50);
-        course.getActivity("FinalExam").setMark(100);
-
-        assertEquals(80, course.calculateTermMark(), 0.02);
+        //Verification..
+        assertEquals("Failed to calculate Course's term mark.", 92, course.calculateTermMark(), 0.02);
     }
 
+    /**
+     *  TODO Assertion Msg. #
+     *  TODO Fixture refactor. #
+     *
+     * @throws CourseActivityException
+     */
     @Test
     public void calculateTermGradeTest() throws CourseActivityException {
-        course.addActivity(CourseActivity.midtermExam(40));
-        course.addActivity(CourseActivity.finalExam(60));
-        course.getActivity("MidtermExam").setMark(50);
-        course.getActivity("FinalExam").setMark(100);
+        //Fixture setup..
+        activityMidterm.setMark(80);
+        activityFinal.setMark(100);
+        course.addActivity(activityMidterm);
+        course.addActivity(activityFinal);
 
-        assertEquals("BB", course.calculateTermGrade());
+        //Verification..
+        assertEquals("AA", course.calculateTermGrade());
     }
 
 }
